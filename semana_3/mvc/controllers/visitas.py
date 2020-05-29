4a732d9af2963307fd3ceeb42e67070a120729bf
yuli05 <1718110384@utectulancingo.edu.mx>
2020-05-29 08:52:04 +0000

    cookies with date and time

--- semana_3/mvc/controllers/visitas.py
+++ semana_3/mvc/controllers/visitas.py
@@ -1,10 +1,14 @@
 import web
+from datetime import datetime
 
 class Visitas:
     def GET(self, nombre):
       try:
           cookie = web.cookies()
           visitas = "0"
+          #fecha actual
+          now = datetime.now()
+
           print(cookie)
           if nombre:
               web.setcookie("nombre",nombre,expires="", domain=None)
@@ -20,6 +24,6 @@ class Visitas:
               web.setcookie("visitas", str(1), expires="", domain=None)
               visitas = "1"
 
-          return "Visitas " + str(visitas) + " Nombre " + nombre 
+          return "Visitas " + str(visitas) + " Nombre " + nombre + " La fecha y hora de vita es: " + str(now) 
       except Exception as e:
         return "Error " + str(e.args)
